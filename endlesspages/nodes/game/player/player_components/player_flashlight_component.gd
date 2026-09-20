0fd361e51fbc1002d5912e3c3a1496dc3b61e40e
Matthew O'Neill <matthew.oneill@mymail.champlain.edu>
2026-09-20 02:04:41 -0400

    feat: tween instead of lerp for flashlight

--- endlesspages/nodes/game/player/player_components/player_flashlight_component.gd
+++ endlesspages/nodes/game/player/player_components/player_flashlight_component.gd
@@ -3,29 +3,26 @@ extends PlayerComponent
 ## Handles all actions relating to rotating the player's flashlight.
 
 @export_group("Interpolation")
-## Vertical, Horizontal
-@export var _lerp_speed: Vector2 = Vector2(40.0, 20.0)
+@export var _camera_follow_lerp_speed: float = 20.0
 
-@export_group("Mouse Offset", "_mouse_offset")
-@export var _mouse_offset_lerp_speed: float = 15.0
-@export var _mouse_offset_dampener: Vector2 = Vector2(100.0, 300.0)
+@export_group("Tweening")
+@export var _run_settings: TweenSettings
 
 @export_group("References")
 @export var _flashlight_base: Node3D
-@export var _offset_node: Node3D
+@export var _run_offset_node: Node3D
 @export var _camera: PhantomCamera3D
 
 var _input: InputComponent
-
-var _target_rotation: Vector3
 var _attractor: Node3D = null # TODO: Replace type with a FlashlightAttractionArea :)
 
-var _mouse_movement_offset: Vector2 = Vector2(0.0, 0.0)
+var _run_tween: Tween
+var _run_weight: float = 0.0
 
 const ROTATION_BOUNDS_X: Vector2 = Vector2(-89, 89)
-const MOUSE_OFFSET_BOUNDS_X: Vector2 = Vector2(-20, 20)
-const MOUSE_OFFSET_BOUNDS_Y: Vector2 = Vector2(-12, 12)
 const RUN_OFFSET_X: float = -45.0
+const RUN_WEIGHT_MIN: float = -0.1  # small dip allowed
+const RUN_WEIGHT_MAX: float = 1.15  # small overshoot allowed
 
 
 func init() -> void:
@@ -33,30 +30,26 @@ func init() -> void:
 	
 	_input = _host.get_component(InputComponent)
 	
-	create_bind(_input.mouse_motion, _on_input_mouse_motion)
+	create_bind(_player.movement_state.changed, _on_player_movement_state_changed)
 
 
 func tick(delta: float) -> void:
-	_target_rotation = _get_offset_rotation_target()
-	_flashlight_base.global_rotation.x = lerp_angle(_flashlight_base.global_rotation.x, _target_rotation.x, _get_lerp_delta(_lerp_speed.x, delta))
-	_flashlight_base.global_rotation.y = lerp_angle(_flashlight_base.global_rotation.y, _target_rotation.y, _get_lerp_delta(_lerp_speed.y, delta))
+	_follow_camera(delta)
+	_run_offset_node.rotation.x = deg_to_rad(RUN_OFFSET_X) * clampf(_run_weight, RUN_WEIGHT_MIN, RUN_WEIGHT_MAX)
 	
 	# Clamp X to not overshoot
 	_flashlight_base.global_rotation.x = deg_to_rad(clampf(_flashlight_base.global_rotation_degrees.x, ROTATION_BOUNDS_X.x, ROTATION_BOUNDS_X.y))
-	
-	# Add mouse movement offset to rotation
-	_flashlight_base.global_rotation += Vector3(deg_to_rad(-_mouse_movement_offset.y), deg_to_rad(-_mouse_movement_offset.x), 0.0)
-	
-	# Lerp mouse offset to zero
-	_mouse_movement_offset = _mouse_movement_offset.lerp(Vector2.ZERO,_get_lerp_delta(_mouse_offset_lerp_speed, delta))
 
 
-func _follow_camera() -> void:
-	_target_rotation = _camera.global_rotation
-	_flashlight_base.global_rotation = _target_rotation
+func _follow_camera(delta: float) -> void:
+	var target: Node3D = _camera
 	
-	# Add mouse movement offset to rotation
-	_flashlight_base.global_rotation += Vector3(deg_to_rad(-_mouse_movement_offset.y), deg_to_rad(-_mouse_movement_offset.x), 0.0)
+	var current_quat := _flashlight_base.global_transform.basis.get_rotation_quaternion()
+	var target_quat := target.global_transform.basis.get_rotation_quaternion()
+	
+	var interp := current_quat.slerp(target_quat, _get_lerp_delta(_camera_follow_lerp_speed, delta))
+	
+	_flashlight_base.global_transform.basis = Basis(interp)
 
 
 func _get_offset_rotation_target() -> Vector3:
@@ -65,20 +58,24 @@ func _get_offset_rotation_target() -> Vector3:
 	return _camera.global_rotation
 
 
-func _rotate_to_target(target: Vector3, delta: float) -> Vector3:
-	var x := lerp_angle(_target_rotation.x, target.x, _get_lerp_delta(_lerp_speed.x, delta))
-	var y := lerp_angle(_target_rotation.y, target.y, _get_lerp_delta(_lerp_speed.y, delta))
+func _tween_run_weight(target: float) -> void:
+	if _run_tween:
+		_run_tween.kill()
 	
-	return Vector3(x, y, 0.0)
-
-
-func _on_input_mouse_motion(relative: Vector2) -> void:
-	if not enabled:
+	var distance := absf(target - _run_weight)
+	if is_zero_approx(distance):
 		return
 	
-	_mouse_movement_offset += relative / _mouse_offset_dampener
-	_mouse_movement_offset.x = clampf(_mouse_movement_offset.x, MOUSE_OFFSET_BOUNDS_X.x, MOUSE_OFFSET_BOUNDS_X.y)
-	_mouse_movement_offset.y = clampf(_mouse_movement_offset.y, MOUSE_OFFSET_BOUNDS_Y.x, MOUSE_OFFSET_BOUNDS_Y.y)
+	_run_tween = create_tween()
+	_run_tween.tween_property(self, "_run_weight", target, _run_settings.time * distance)\
+		.set_ease(_run_settings.ease).set_trans(_run_settings.trans)
+
+
+func _on_player_movement_state_changed(from: int, to: int) -> void:
+	if from == PlayerMovementComponent.MovementState.RUNNING:
+		_tween_run_weight(0.0)
+	elif to == PlayerMovementComponent.MovementState.RUNNING:
+		_tween_run_weight(1.0)
 
 
 func _get_lerp_delta(speed: float, delta: float) -> float:
---
+++ endlesspages/resources/tween_settings/tween_settings.gd
@@ -0,0 +1,12 @@
+class_name TweenSettings
+extends Resource
+## Easier way to store Tween related settings.
+##
+## Things like time, easing, transition.
+
+@export var time: float = 0.5
+@warning_ignore("shadowed_global_identifier")
+@export var ease: Tween.EaseType = Tween.EASE_OUT
+@export var trans: Tween.TransitionType = Tween.TRANS_QUART
+## Cooldown before this tween can be started again.
+@export var cooldown: float = 0.5
